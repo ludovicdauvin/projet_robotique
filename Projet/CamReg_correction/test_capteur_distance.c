a3c657bdfda75a2fb8cff0551e6ea0aba2a97757
ludovicdauvin <ludovic.dauvin@epfl.ch>
2021-04-20 10:14:19 +0200

    suite

---
+++ Projet/CamReg_correction/test_capteur_distance.c
@@ -0,0 +1,66 @@
+/*
+ * test_capteur_distance.c
+ *
+ *  Created on: 15 avr. 2021
+ *      Author: Ludovic
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include <chprintf.h>
+#include <usbcfg.h>
+
+#include <main.h>
+#include <test_capteur_distance.h>
+#include <leds.h>
+#include <sensors/proximity.h>
+
+static THD_WORKING_AREA(waTestDistance, 512);
+static THD_FUNCTION(TestDistance, arg) {
+
+    chRegSetThreadName(__FUNCTION__);
+    (void)arg;
+
+    systime_t time;
+    calibrate_ir();
+
+    messagebus_topic_t *prox_topic = messagebus_find_topic_blocking(&bus, "/proximity");
+      proximity_msg_t prox_values;
+
+    int luminosite=0;
+
+//    while(1){
+//        time = chVTGetSystemTime();
+//
+//        luminosite = get_prox(0);
+//
+//        if(luminosite<get_ambient_light(0)){
+//
+//        set_body_led(TRUE); // value (0=off 1=on higher=inverse)
+//        }else{
+//        	 set_body_led(FALSE); // value (0=off 1=on higher=inverse)
+//        }
+//        //100Hz
+//        chThdSleepUntilWindowed(time, time + MS2ST(10));
+//    }
+
+    while(1){
+    	 time = chVTGetSystemTime();
+    	 messagebus_topic_wait(prox_topic, &prox_values, sizeof(prox_values));
+    for (uint8_t i = 0; i < sizeof(prox_values.ambient)/sizeof(prox_values.ambient[0]); i++) {
+  		        //for (uint8_t i = 0; i < PROXIMITY_NB_CHANNELS; i++) {
+  		        	chprintf((BaseSequentialStream *)&SDU1, "%4d,", prox_values.ambient[i]);
+  		        	chprintf((BaseSequentialStream *)&SDU1, "%4d,", prox_values.reflected[i]);
+  		        	chprintf((BaseSequentialStream *)&SDU1, "%4d", prox_values.delta[i]);
+  		        	chprintf((BaseSequentialStream *)&SDU1, "\r\n");
+  		        }
+    chThdSleepUntilWindowed(time, time + MS2ST(10));
+}
+}
+void test_distance_start(void){
+	chThdCreateStatic(waTestDistance, sizeof(waTestDistance), NORMALPRIO, TestDistance, NULL);
+}
---
+++ Projet/CamReg_correction/test_capteur_distance.h
@@ -0,0 +1,16 @@
+/*
+ * test_capteur_distance.h
+ *
+ *  Created on: 15 avr. 2021
+ *      Author: Ludovic
+ */
+
+#ifndef TEST_CAPTEUR_DISTANCE_H_
+#define TEST_CAPTEUR_DISTANCE_H_
+
+
+void test_distance_start(void);
+
+
+
+#endif /* TEST_CAPTEUR_DISTANCE_H_ */
