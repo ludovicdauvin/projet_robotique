27fdcd7ab17e650bc4d2aa9a85c1e5602ffbc55f
ludovicdauvin <ludovic.dauvin@epfl.ch>
2021-04-20 10:13:52 +0200

    lecture des capteurs IR sur terminal

--- Projet/CamReg_correction/main.c
+++ Projet/CamReg_correction/main.c
@@ -8,31 +8,41 @@
 #include "memory_protection.h"
 #include <usbcfg.h>
 #include <main.h>
-#include <motors.h>
-#include <camera/po8030.h>
+//#include <motors.h>
+//#include <camera/po8030.h>
 #include <chprintf.h>
 
-#include <pi_regulator.h>
-#include <process_image.h>
+//#include <pi_regulator.h>
+//#include <process_image.h>
 
-void SendUint8ToComputer(uint8_t* data, uint16_t size) 
-{
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
-}
+//nos includes ajouté
+#include <test_capteur_distance.h>
+#include <leds.h>
+#include <sensors/proximity.h>
 
-static void serial_start(void)
-{
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
 
-	sdStart(&SD3, &ser_cfg); // UART3.
-}
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+//void SendUint8ToComputer(uint8_t* data, uint16_t size)
+//{
+//	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
+//	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
+//	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
+//}
+
+//static void serial_start(void)
+//{
+//	static SerialConfig ser_cfg = {
+//	    115200,
+//	    0,
+//	    0,
+//	    0,
+//	};
+//
+//	sdStart(&SD3, &ser_cfg); // UART3.
+//}
 
 int main(void)
 {
@@ -41,22 +51,35 @@ int main(void)
     chSysInit();
     mpu_init();
 
+    messagebus_init(&bus, &bus_lock, &bus_condvar);
+
+
     //starts the serial communication
-    serial_start();
+//    serial_start();
     //start the USB communication
     usb_start();
+
+    //starts the proximity sensors
+    proximity_start();
+    //calibrate proximity sensors
+
+
+
     //starts the camera
-    dcmi_start();
-	po8030_start();
-	//inits the motors
-	motors_init();
+//    dcmi_start();
+//	po8030_start();
+//	//inits the motors
+//	motors_init();
 
 	//stars the threads for the pi regulator and the processing of the image
-	pi_regulator_start();
-	process_image_start();
+//	pi_regulator_start();
+//	process_image_start();
 
+    //test capteur distance
+    test_distance_start();
     /* Infinite loop. */
     while (1) {
+
     	//waits 1 second
         chThdSleepMilliseconds(1000);
     }
